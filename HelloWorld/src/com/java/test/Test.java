3f43a3dd26aa775d55adb782f031f919228e6186
benjamin <benjamin@WINDOWS-CL23D6D>
2018-05-28 22:59:55 -0400

    change

--- HelloWorld/src/com/java/test/Test.java
+++ HelloWorld/src/com/java/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 		
 		public static void main(String[] args) {
 			System.out.println("Hello Git");
-			System.out.println("Hello Git");
-			System.out.println("Hello Git");
+			System.out.println("Wert");
+			System.out.println("4567");
 			System.out.println("Hello Git");
 		}
 	
